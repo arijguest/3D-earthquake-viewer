1700c1bf2e91a605230878193c349eacc6a8c00c
arijguest <117730068+arijguest@users.noreply.github.com>
2024-11-13 13:40:59 +0000

    Fixed a silly mistake...

--- 3D-EQ.py
+++ 3D-EQ.py
@@ -560,9 +560,5 @@ HTML_TEMPLATE = """
 def index():
     return render_template_string(HTML_TEMPLATE, cesium_token=CESIUM_ION_ACCESS_TOKEN)
 
-def run_app():
-    port = int(os.environ.get('PORT', 8006))
-    app.run(host='0.0.0.0', port=port)
-
 if __name__ == "__main__":
-    Thread(target=run_app).start()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8006)))
